e5ea81b5ca3d2203aa6b14c88b46d85e6a25bba1
Lingyi Zhang <Lingyi@Lingyis-MBP.fios-router.home>
2018-02-10 18:56:41 -0500

    init

---
+++ AWS_API_Gateway/lambda_function/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+exports.handler = (event, context, callback) => {
+    let long = event.long;
+    let lati = event.lati;
+    
+    var querypoint = 'Point(' + long + ' ' + lati + ')';
+    // var querypoint = 'Point(-118.159953387143 33.8719625444075)';
+    const promise = require('bluebird'); // or any other Promise/A+ compatible library;
+    const initOptions = {
+        promiseLib: promise // overriding the default (ES6 Promise);
+    };
+    const pgp = require('pg-promise')(initOptions);
+    const connection = "postgres://gauravbhardwaj:argolabs@db2.c8qdkldhfwra.us-east-1.rds.amazonaws.com:5432/db_argo";
+    const db = pgp(connection);
+    console.log("Start Query");
+    var querytext = "SELECT * FROM db_argo WHERE ST_Contains (db_argo.the_geom, ST_GeomFromText('" + querypoint + "', 4326))=TRUE;";
+    console.log(querytext);
+    db.any(querytext, [true])
+        .then(data => {
+            console.log('DATA:', data); // print data;
+            callback(null, data);
+        })
+        .catch(error => {
+            console.log('ERROR:', error); // print the error;
+        })
+        .finally(db.$pool.end);
+};
