0b0917f0d43f303e90796238ae02cf8e81c69360
lingyielia <31973683+lingyielia@users.noreply.github.com>
2018-03-27 13:42:12 -0400

    update

--- APP/app.js
+++ APP/app.js
@@ -17,19 +17,19 @@ app.get("/b", function(req, res){
 
 // geocode first, query RDS second
 app.get("/addr-results", function(req, res){
-    
+
     var queryaddr = req.query.physaddress;
     var replaced = queryaddr.split(' ').join('+');
-    var apiurl = "https://maps.googleapis.com/maps/api/geocode/json?address="+replaced+"&key=AIzaSyCzl4xYox3YGA3g8u553Im6rP784cTHXBo";
+    var apiurl = "https://maps.googleapis.com/maps/api/geocode/json?address="+replaced+"&key=Theapikey";
 
     fetch(apiurl).then(function(response) {
       return response.json();
     }).then(function(data) {
-        
+
       var backlon = data.results[0].geometry.location.lng;
       var backlat = data.results[0].geometry.location.lat;
       var fulladdr = data.results[0].formatted_address;
-    
+
       var queryplace = req.query.place;
       var querypoint = 'Point(' + backlon + ' ' + backlat + ')';
       const promise = require('bluebird'); // or any other Promise/A+ compatible library;
@@ -37,7 +37,7 @@ app.get("/addr-results", function(req, res){
         promiseLib: promise // overriding the default (ES6 Promise);
       };
       const pgp = require('pg-promise')(initOptions);
-      const connection = "postgres://gbhardwaj:argolabs@geocrosswalk.c8idj0wb3ddk.us-east-1.rds.amazonaws.com:5432/crosswalk";
+      const connection = "postgres://username:password@geocrosswalk.c8idj0wb3ddk.us-east-1.rds.amazonaws.com:5432/crosswalk";
       const db = pgp(connection);
       console.log("Start Query");
       if (queryplace == "nyc") {
@@ -45,7 +45,7 @@ app.get("/addr-results", function(req, res){
       } else {
         var querytext = "SELECT * FROM latable WHERE ST_Contains (latable.the_geom, ST_GeomFromText('" + querypoint + "', 4326))=TRUE;";
       };
-    
+
       console.log(querytext);
       db.any(querytext, [true])
         .then(data => {
@@ -55,14 +55,14 @@ app.get("/addr-results", function(req, res){
             } else {
                 res.render("geocrosswalkLAa", {fulladdr: fulladdr, data: data});
             }
-            
+
         })
         .catch(error => {
             console.log('ERROR:', error); // print the error;
         })
-        .finally(db.$pool.end); // For immediate app exit, shutting down the connection pool    
-        
-        
+        .finally(db.$pool.end); // For immediate app exit, shutting down the connection pool
+
+
 
     }).catch(function() {
       console.log("Booo");
@@ -73,7 +73,7 @@ app.get("/addr-results", function(req, res){
 
 // query from AWS RDS directly
 app.get("/results", function(req, res){
-    
+
     var queryplace = req.query.place;
     console.log(queryplace);
     var querypoint = 'Point(' + req.query.lonSearch + ' ' + req.query.latSearch + ')';
@@ -82,7 +82,7 @@ app.get("/results", function(req, res){
         promiseLib: promise // overriding the default (ES6 Promise);
     };
     const pgp = require('pg-promise')(initOptions);
-    const connection = "postgres://gbhardwaj:argolabs@geocrosswalk.c8idj0wb3ddk.us-east-1.rds.amazonaws.com:5432/crosswalk";
+    const connection = "postgres://username:password@geocrosswalk.c8idj0wb3ddk.us-east-1.rds.amazonaws.com:5432/crosswalk";
     const db = pgp(connection);
     console.log("Start Query");
     if (queryplace == "nyc") {
@@ -90,20 +90,20 @@ app.get("/results", function(req, res){
     } else {
         var querytext = "SELECT * FROM latable WHERE ST_Contains (latable.the_geom, ST_GeomFromText('" + querypoint + "', 4326))=TRUE;";
     };
-    
+
     console.log(querytext);
     db.any(querytext, [true])
         .then(data => {
             console.log('DATA:', data); // print data;
             var long = req.query.lonSearch;
             var lati = req.query.latSearch;
-            
+
             if (queryplace == "nyc") {
                 res.render("geocrosswalkNYCb", {longVar: long, latiVar: lati, data: data});
             } else {
                 res.render("geocrosswalkLAb", {longVar: long, latiVar: lati, data: data});
             }
-            
+
         })
         .catch(error => {
             console.log('ERROR:', error); // print the error;
@@ -125,4 +125,3 @@ app.get("*", function(req, res) {
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server started!");
 });
-
